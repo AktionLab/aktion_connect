d7aba8eff88179616d8f8cf0da46745fff862b85
Chris Boertien <chris@aktionlab.com>
2014-02-14 06:21:12 -0700

    test that the override configs work
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- features/configure_connection.feature
+++ features/configure_connection.feature
@@ -10,8 +10,22 @@ Feature: Connect to a confgiured service
     When a connection to the service is requested
     Then the service receives a connection request with the default config
 
+  Scenario: Establish a connection to a configured service using an override config
+    Given the service has a default config
+    And the service has an override config
+    And the service has been configured
+    When a connection to the service is requested
+    Then the service receives a connection request with the override config
+
   Scenario: Establish a connection to a configured service using an environment
     Given the service has a default config with environments
     And the service has been configured with a "test" environment
     When a connection to the service is requested
     Then the service receives a connection request with the "test" default config
+
+  Scenario: Establish a connection to a configured service using an override with environment
+    Given the service has a default config with environments
+    And the service has an override config with environments
+    And the service has been configured with a "test" environment
+    When a connection to the service is requested
+    Then the service receives a connection request with the "test" override config
--- features/step_definitions/aktion_connect.rb
+++ features/step_definitions/aktion_connect.rb
@@ -1,3 +1,8 @@
+After do
+  FileUtils.rm_r('config') if Dir.exists?('config')
+  Aktion::Connect.env = nil
+end
+
 Given /^a config directory is setup$/ do
   FileUtils.mkdir_p('config')
   config_dir = File.expand_path(File.join(__FILE__,'..','..','..','config'))
@@ -32,6 +37,13 @@ Given /^the service has a default config$/ do
   YML
 end
 
+Given /^the service has an override config$/ do
+  File.write("config/test.yml", <<-YML)
+    user: 'ouser'
+    pass: 'opass'
+  YML
+end
+
 Given /^the service has a default config with environments$/ do
   File.write('config/test.defaults.yml', <<-YML)
     development:
@@ -43,6 +55,17 @@ Given /^the service has a default config with environments$/ do
   YML
 end
 
+Given /^the service has an override config with environments$/ do
+  File.write('config/test.defaults.yml', <<-YML)
+    development:
+      user: 'odev_user'
+      pass: 'odev_pass'
+    test:
+      user: 'otest_user'
+      pass: 'otest_pass'
+  YML
+end
+
 When /^a connection to the service is requested$/ do
   Aktion::Connect.connect('test')
 end
@@ -51,11 +74,25 @@ Then /^the service receives a connection request with the default config$/ do
   TestService.should have_received(:connect).with({'user'=>'user','pass'=>'pass'})
 end
 
+Then /^the service receives a connection request with the override config$/ do
+  TestService.should have_received(:connect).with({'user'=>'ouser','pass'=>'opass'})
+end
+
 Then /^the service receives a connection request with the "([^"]+)" default config$/ do |env|
   if env == 'development'
     TestService.should have_received(:connect).with({'user'=>'dev_user','pass'=>'dev_pass'})
   elsif env == 'test'
     TestService.should have_received(:connect).with({'user'=>'test_user','pass'=>'test_pass'})
+  else
+    raise 'unknown env'
+  end
+end
+
+Then /^the service receives a connection request with the "([^"]+)" override config$/ do |env|
+  if env == 'development'
+    TestService.should have_received(:connect).with({'user'=>'odev_user','pass'=>'odev_pass'})
+  elsif env == 'test'
+    TestService.should have_received(:connect).with({'user'=>'otest_user','pass'=>'otest_pass'})
   else
     raise 'unknown env'
   end
