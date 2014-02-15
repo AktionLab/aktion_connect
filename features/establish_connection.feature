d3dc87e96480bf8b348f40e2790bf01805daee0c
Chris Boertien <chris@aktionlab.com>
2014-02-15 06:13:46 -0700

    move config file steps to their own feature
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- features/establish_connection.feature
+++ features/establish_connection.feature
@@ -4,46 +4,6 @@ Feature: Connect to a service
     Given there is a service
     And a config directory is setup
 
-  Scenario: Establish a connection using a default config
-    Given the service has a default config
-    And the service has been configured
-    When a connection to the service is requested
-    Then the service receives a connection request with the default config
-
-  Scenario: Establish a connection using an override config
-    Given the service has a default config
-    And the service has an override config
-    And the service has been configured
-    When a connection to the service is requested
-    Then the service receives a connection request with the override config
-
-  Scenario: Establish a connection using a partial override config
-    Given the service has a default config
-    And the service has a partial override config
-    And the service has been configured
-    When a connection to the service is requested
-    Then the service receives a connection request with the partial override config
-
-  Scenario: Establish a connection using an environment
-    Given the service has a default config with environments
-    And the service has been configured with a "test" environment
-    When a connection to the service is requested
-    Then the service receives a connection request with the "test" default config
-
-  Scenario: Establish a connection using an override with environment
-    Given the service has a default config with environments
-    And the service has an override config with environments
-    And the service has been configured with a "test" environment
-    When a connection to the service is requested
-    Then the service receives a connection request with the "test" override config
-
-  Scenario: Establish a connection using an override with environment
-    Given the service has a default config with environments
-    And the service has a partial override config with environments
-    And the service has been configured with a "test" environment
-    When a connection to the service is requested
-    Then the service receives a connection request with the "test" partial override config
-
   Scenario: Establish a connection using a connect method by default
     Given the service has a default config
     And the service has been configured without a block
--- features/step_definitions/establish_connection.rb
+++ features/step_definitions/establish_connection.rb
@@ -31,103 +31,10 @@ Given /^the service has been configured with a "([^"]+)" environment$/ do |env|
   step "the service has been configured"
 end
 
-Given /^the service has a default config$/ do
-  File.write('config/test_service.defaults.yml', <<-YML)
-    user: 'user'
-    pass: 'pass'
-  YML
-end
-
-Given /^the service has an override config$/ do
-  File.write("config/test_service.yml", <<-YML)
-    user: 'ouser'
-    pass: 'opass'
-  YML
-end
-
-Given /^the service has a partial override config$/ do
-  File.write("config/test_service.yml", <<-YML)
-    user: 'ouser'
-  YML
-end
-
-Given /^the service has a default config with environments$/ do
-  File.write('config/test_service.defaults.yml', <<-YML)
-    development:
-      user: 'dev_user'
-      pass: 'dev_pass'
-    test:
-      user: 'test_user'
-      pass: 'test_pass'
-  YML
-end
-
-Given /^the service has an override config with environments$/ do
-  File.write('config/test_service.yml', <<-YML)
-    development:
-      user: 'odev_user'
-      pass: 'odev_pass'
-    test:
-      user: 'otest_user'
-      pass: 'otest_pass'
-  YML
-end
-
-Given /^the service has a partial override config with environments$/ do
-  File.write('config/test_service.yml', <<-YML)
-    development:
-      user: 'odev_user'
-    test:
-      user: 'otest_user'
-  YML
-end
-
 When /^a connection to the service is requested$/ do
   Aktion::Connect.connect('test_service')
 end
 
 Then /^the service receives a connection request$/ do
   TestService.should have_received(:connect)
-end
-
-Then /^the service receives a connection request with the default config$/ do
-  TestService.should have_received(:connect).with({'user'=>'user','pass'=>'pass'})
-end
-
-Then /^the service receives a connection request with the override config$/ do
-  TestService.should have_received(:connect).with({'user'=>'ouser','pass'=>'opass'})
-end
-
-Then /^the service receives a connection request with the partial override config$/ do
-  TestService.should have_received(:connect).with({'user'=>'ouser','pass'=>'pass'})
-end
-
-Then /^the service receives a connection request with the "([^"]+)" default config$/ do |env|
-  if env == 'development'
-    TestService.should have_received(:connect).with({'user'=>'dev_user','pass'=>'dev_pass'})
-  elsif env == 'test'
-    TestService.should have_received(:connect).with({'user'=>'test_user','pass'=>'test_pass'})
-  else
-    raise 'unknown env'
-  end
-end
-
-Then /^the service receives a connection request with the "([^"]+)" override config$/ do |env|
-  if env == 'development'
-    TestService.should have_received(:connect).with({'user'=>'odev_user','pass'=>'odev_pass'})
-  elsif env == 'test'
-    TestService.should have_received(:connect).with({'user'=>'otest_user','pass'=>'otest_pass'})
-  else
-    raise 'unknown env'
-  end
-end
-
-Then /^the service receives a connection request with the "([^"]+)" partial override config$/ do |env|
-  if env == 'development'
-    TestService.should have_received(:connect).with({'user'=>'odev_user','pass'=>'dev_pass'})
-  elsif env == 'test'
-    TestService.should have_received(:connect).with({'user'=>'otest_user','pass'=>'test_pass'})
-  else
-    raise 'unknown env'
-  end
 end
